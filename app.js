7b065bfd37aff6bd8f9c2f60c758ce992bfc37b1
unknown <engrnasirs18@gmail.com>
2020-07-26 16:04:27 +0500

    Now ready to commit

---
+++ app.js
@@ -0,0 +1,50 @@
+var list = document.getElementById("list")
+
+function addTodo() {
+      var  todo_item = document.getElementById("todo-item")
+      var li = document.createElement('li')
+      var liText = document.createTextNode(todo_item.value)
+      li.appendChild(liText)
+      
+      var delBtn = document.createElement("Button")
+      var delText = document.createTextNode("DELETE")
+      delBtn.setAttribute("class","btn")
+      delBtn.setAttribute("onclick","deleteItem(this)")
+      delBtn.appendChild(delText)
+
+// creat edite button
+var editBtn = document.createElement("Button") 
+var editText = document.createTextNode("EDIT")
+editBtn.appendChild(editText)
+li.appendChild(delBtn)
+li.appendChild(editBtn)
+editBtn.setAttribute("onclick","editItem(this)")
+
+
+
+
+      li.appendChild(delBtn )
+      
+      list.appendChild(li)
+      todo_item.value = ""
+     
+
+}
+
+
+
+function deleteItem(e){
+         e.parentNode.remove()
+}
+function editItem(e){
+    var val = e.parentNode.firstChild.nodeValue;
+    var editValue = prompt("Enter Edit Value",val)
+    e.parentNode.firstChild.nodeValue = editValue
+}
+
+
+
+
+function deleteAll(){
+    list.innerHTML = " "
+}
